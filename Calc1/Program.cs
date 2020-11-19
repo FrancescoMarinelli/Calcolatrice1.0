51488655f538b9dbe282976dccb6fb79e8349c5d
francesco marinelli <francescomarinelli04@gmail.com>
2020-11-19 08:27:35 +0100

    Aggiungere i file di progetto.

---
+++ Calc1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LibrerieOperazioni;
+
+namespace Calc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = Operazioni.Numero();
+            double n2 = Operazioni.Numero();
+            double? risultato = Operazioni.LibrerieOperazioni(n1, n2);
+            Console.WriteLine($"{risultato}");
+        }
+    }
+}
+
+
+
---
+++ LibrerieOperazioni/Operazioni.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LibrerieOperazioni
+{
+    public class Operazioni
+    {
+        public static double Numero()
+        {
+            Console.Write("inserisci un numero: ");
+            double n = double.Parse(Console.ReadLine());
+            return n;
+        }
+
+        public static double? LibrerieOperazioni(double n1, double n2)
+        {
+
+            double? risultato = null;
+            Console.WriteLine("1=somma, 2=differenza, 3=moltiplicazione, 4=divisione");
+            Console.WriteLine("seleziona l'operazione ");
+            string str = Console.ReadLine();
+            switch (str)
+            {
+                case "A":
+                case "Somma":
+                    risultato = n1 + n2;
+                    break;
+                case "B":
+                case "Differenza":
+                    risultato = n1 - n2;
+                    break;
+                case "C":
+                case "Moltiplicazione":
+                    risultato = n1 * n2;
+                    break;
+                case "D":
+                case "Divisione":
+                    risultato = n1 / n2;
+                    break;
+                default:
+                    Console.WriteLine("errore non hai eseguito nessuna operazione");
+                    break;
+            }
+            return risultato;
+        }
+    }
+}
+
